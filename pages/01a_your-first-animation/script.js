c988793309ce6511d537263180f7290554820a79
annafilenko-front <anna.filenko.dev@gmail.com>
2026-07-11 12:03:07 +0300

    :dizzy: animated card with yoyo effect

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,16 @@
+import gsap from 'gsap'
+
+gsap.to('.card', {
+  opacity: 1,
+  scale: 1,
+  duration: 3,
+  onComplete: () => {
+    gsap.to('.card', {
+      y: -20,
+      boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+      repeat:-1,
+      yoyo: true,
+      duration: 0.5
+    })
+  },
+});
